bcc1962aabc1e7e201e563a820a057a615c8acc7
Margus Kevin Sünter <kevin@synter.ee>
2023-11-24 02:11:24 +0200

    Lint auth store

--- src/stores/auth0.js
+++ src/stores/auth0.js
@@ -1,32 +1,32 @@
-import { defineStore } from 'pinia';
-import webAuth from 'auth0-js';
+import { defineStore } from "pinia";
+import webAuth from "auth0-js";
 
 const auth0Config = {
   domain: import.meta.env.VITE_AUTH0_DOMAIN,
   clientID: import.meta.env.VITE_AUTH0_CLIENT_ID,
   redirectUri: window.location.origin,
   audience: import.meta.env.VITE_APP_AUTH0_AUDIENCE,
-  responseType: 'token id_token',
-  scope: 'openid profile',
-}
+  responseType: "token id_token",
+  scope: "openid profile",
+};
 const auth0Client = new webAuth.WebAuth(auth0Config);
 
-export const useAuth0Store = defineStore('auth0', {
+export const useAuth0Store = defineStore("auth0", {
   state: () => {
     return {
       isAuthenticated: false,
       loading: true,
       userProfile: null,
-      accessToken: '',
-      idToken: '',
-    }
+      accessToken: "",
+      idToken: "",
+    };
   },
 
   getters: {
-   isAuthenticatedCheck() {
+    isAuthenticatedCheck() {
       // Check whether the current time is past the
       // access token's expiry time
-      const expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+      const expiresAt = JSON.parse(localStorage.getItem("expires_at"));
       return new Date().getTime() < expiresAt;
     },
   },
@@ -34,7 +34,7 @@ export const useAuth0Store = defineStore('auth0', {
   actions: {
     async handleAuthentication() {
       return new Promise((resolve, reject) => {
-        auth0Client.parseHash(async(err, authResult) => {
+        auth0Client.parseHash(async (err, authResult) => {
           if (authResult && authResult.accessToken && authResult.idToken) {
             this.setSession(authResult);
             this.userProfile = await this.getUserProfile();
@@ -42,36 +42,37 @@ export const useAuth0Store = defineStore('auth0', {
           } else if (err) {
             reject(err);
           } else {
-            this.isAuthenticated = false
-            resolve()
+            this.isAuthenticated = false;
+            resolve();
           }
         });
-      })
+      });
     },
 
     setSession(authResult) {
       // Set the time that the access token will expire
-      const expiresAt = JSON.stringify(authResult.expiresIn * 1000 + new Date().getTime());
+      const expiresAt = JSON.stringify(
+        authResult.expiresIn * 1000 + new Date().getTime(),
+      );
 
       // Save tokens and expiration to localStorage
-      localStorage.setItem('access_token', authResult.accessToken);
-      localStorage.setItem('id_token', authResult.idToken);
-      localStorage.setItem('expires_at', expiresAt);
-      localStorage.setItem('isAuthenticated', true);
-  
+      localStorage.setItem("access_token", authResult.accessToken);
+      localStorage.setItem("id_token", authResult.idToken);
+      localStorage.setItem("expires_at", expiresAt);
+      localStorage.setItem("isAuthenticated", "true");
+
       this.accessToken = authResult.accessToken;
       this.idToken = authResult.idToken;
       this.isAuthenticated = true;
       this.loading = false;
     },
 
-
     async getUserProfile() {
       if (this.userProfile) {
         return this.userProfile;
       } else {
         return new Promise((resolve, reject) => {
-          let access_token = localStorage.getItem('access_token');
+          let access_token = localStorage.getItem("access_token");
           auth0Client.client.userInfo(access_token, (err, profile) => {
             if (profile) {
               this.userProfile = profile;
@@ -84,51 +85,52 @@ export const useAuth0Store = defineStore('auth0', {
         });
       }
     },
-    async passwordlessLogin(email) {
+    passwordlessLogin(email) {
       return new Promise((resolve, reject) => {
-        auth0Client.passwordlessStart({
-            connection: 'email',
-            send: 'code',
-            email: email
-          }, async function (err,res) {
-            if(err) {
-              reject(err)
-            } else {
-              // route to password field component
-              resolve(res.email)
-            }
-          }
-        );
-      })
-      
-    },
-    async verifyPasswordlessCode(email,code) {
-      return new Promise((resolve, reject) => {
-        auth0Client.passwordlessLogin({
-            connection: 'email',
+        auth0Client.passwordlessStart(
+          {
+            connection: "email",
+            send: "code",
             email: email,
-            verificationCode: code
-          }, function (err,res) {
-            if(err) {
-              reject(err)
+          },
+          function (err, res) {
+            if (err) {
+              reject(err);
             } else {
-              resolve(res)
+              resolve(res.email);
             }
-          }
+          },
         );
-      })
-      
+      });
+    },
+    async verifyPasswordlessCode(email, code) {
+      return new Promise((resolve, reject) => {
+        auth0Client.passwordlessLogin(
+          {
+            connection: "email",
+            email: email,
+            verificationCode: code,
+          },
+          function (err, res) {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(res);
+            }
+          },
+        );
+      });
     },
 
     loginWithGoogle() {
       auth0Client.authorize({
-        connection: 'google-oauth2'
+        connection: "google-oauth2",
       });
     },
 
     loginWithMeta() {
       auth0Client.authorize({
-        connection: 'facebook'
+        connection: "facebook",
       });
     },
 
@@ -138,18 +140,18 @@ export const useAuth0Store = defineStore('auth0', {
 
     logout() {
       // Remove tokens and expiration from localStorage
-      localStorage.removeItem('access_token');
-      localStorage.removeItem('id_token');
-      localStorage.removeItem('expires_at');
-      localStorage.removeItem('isAuthenticated');
+      localStorage.removeItem("access_token");
+      localStorage.removeItem("id_token");
+      localStorage.removeItem("expires_at");
+      localStorage.removeItem("isAuthenticated");
 
-      this.accessToken = '';
-      this.idToken = '';
+      this.accessToken = "";
+      this.idToken = "";
       this.isAuthenticated = false;
 
       auth0Client.logout({
         returnTo: window.location.origin + "/login",
-      })
+      });
     },
   },
 });
