29e73f056238e8f1b8ed95f797c33ace6956718c
Margus Kevin Sünter <kevin@synter.ee>
2023-12-11 08:44:34 +0200

    Add Pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt"],
+  modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt", "@pinia/nuxt"],
   ssr: false,
 });
