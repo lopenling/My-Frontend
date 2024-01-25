673b48271cc9610c63160c2eb5e7450813a50f06
Margus Kevin Sünter <kevin@synter.ee>
2024-01-26 01:13:41 +0200

    Add basic GraphQL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt", "@pinia/nuxt"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@vueuse/nuxt",
+    "@pinia/nuxt",
+    "nuxt-graphql-request",
+  ],
+  build: {
+    transpile: ["nuxt-graphql-request"],
+  },
   ssr: false,
+
+  graphql: {
+    clients: {
+      default: {
+        endpoint: "http://localhost:8080/v1/graphql",
+        // httpEndpoint: "http://localhost:8080/v1/graphql",
+      },
+    },
+  },
+  alias: {
+    "cross-fetch": "cross-fetch/dist/browser-ponyfill.js",
+  },
 });
