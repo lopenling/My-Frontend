9432f25afcc7da59355e281a6134090458894557
Margus Kevin Sünter <kevin@synter.ee>
2024-01-26 02:24:27 +0200

    Add password login

---
+++ pages/login/password.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
+    <!-- Form -->
+    <div class="flex flex-col">
+      <form action="/dashboard" method="POST">
+        <FormInput
+          id="password"
+          v-model="password"
+          label="Enter password"
+          class="mb-8"
+          type="password"
+          :required="true"
+          :autofocus="true"
+        />
+        <!-- <InputGroup
+            attributes={{ class: `mb-4` }}
+            label={{ text: "Enter password" }}
+            input={{
+              attributes: {
+                name: "password",
+                id: "password",
+                type: "password",
+                required: "",
+                autofocus: "",
+              },
+              appearance: "white",
+            }}
+          /> -->
+
+        <div class="mb-8 flex items-center">
+          <input
+            id="use-password-sign-in"
+            name="use-password-sign-in"
+            type="checkbox"
+            checked
+            class="h-4 w-4 rounded border-stone-300 text-primary-600 focus:ring-primary-600"
+          />
+          <label
+            for="use-password-sign-in"
+            class="ml-3 block text-sm leading-6 text-stone-900"
+          >
+            Use password sign-in by default
+          </label>
+        </div>
+
+        <div class="mb-8">
+          <button
+            type="submit"
+            class="flex w-full justify-center rounded-md bg-primary-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm transition hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
+          >
+            Sign in
+          </button>
+        </div>
+      </form>
+
+      <div class="text-left text-sm">
+        <a
+          href="/forgot-password"
+          class="font-normal text-primary-600 underline decoration-transparent underline-offset-2 outline-none transition hover:text-primary-500 hover:decoration-primary-500/25 focus-visible:rounded-sm focus-visible:outline focus-visible:outline-offset-2"
+        >
+          Forgot password?
+        </a>
+      </div>
+    </div>
+
+    <!-- Divider -->
+    <div class="relative mt-6">
+      <div class="absolute inset-0 flex items-center" aria-hidden="true">
+        <div class="w-full border-t border-stone-200"></div>
+      </div>
+      <div class="font-regular relative flex justify-center text-sm leading-6">
+        <span class="bg-white px-6 text-stone-500">Or</span>
+      </div>
+    </div>
+
+    <!-- Button -->
+    <div class="mt-6">
+      <button
+        onclick="window.location.href='/sign-in/check-inbox-or-password-link';"
+        type="button"
+        class="flex w-full items-center justify-center gap-3 rounded bg-white px-2 py-1 text-sm font-semibold text-stone-500 shadow-sm ring-1 ring-inset ring-stone-300 transition hover:bg-stone-50 hover:text-stone-700 focus-visible:outline focus-visible:-outline-offset-1"
+      >
+        <!-- <Icon
+            data={IconDataWandMagicSparkles}
+            attributes={{ class: `h-4 w-4` }}
+          /> -->
+        <span class="text-sm font-semibold leading-6">
+          Sign in with magic link
+        </span>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+buildHead({
+  title: "Sign in",
+});
+
+definePageMeta({
+  layout: "auth",
+});
+
+const password = ref("");
+const email = useRoute().query.email;
+</script>
+
+<style lang="scss" scoped></style>
