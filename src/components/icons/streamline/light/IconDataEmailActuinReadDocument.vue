3a104319f289e3eacede5d060beeb322e2bc2384
Margus Kevin Sünter <kevin@synter.ee>
2024-03-04 17:34:14 +0200

    Move icons to subfolders

---
+++ src/components/icons/streamline/light/IconDataEmailActuinReadDocument.vue
@@ -0,0 +1,83 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    stroke="currentColor"
+    viewBox="-0.5 -0.5 24 24"
+    aria-hidden="true"
+    class="h-16 w-16"
+  >
+    <path
+      d="m18.6875 8.385416666666668 3.8333333333333335 2.635416666666667V21.083333333333336a1.4375 1.4375 0 0 1 -1.4375 1.4375H1.9166666666666667A1.4375 1.4375 0 0 1 0.4791666666666667 21.083333333333336V11.020833333333334l3.8333333333333335 -2.635416666666667"
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="1"
+    ></path>
+    <path
+      d="m3.354166666666667 19.645833333333336 4.791666666666667 -3.8333333333333335 6.708333333333334 0 4.791666666666667 3.8333333333333335"
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="1"
+    ></path>
+    <path
+      d="m22.520833333333336 11.020833333333334 -5.75 3.8333333333333335"
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="1"
+    ></path>
+    <path
+      d="m0.4791666666666667 11.020833333333334 5.75 3.8333333333333335"
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="1"
+    ></path>
+    <path
+      d="M18.6875 13.416666666666668V1.4375a0.9583333333333334 0.9583333333333334 0 0 0 -0.9583333333333334 -0.9583333333333334h-12.458333333333334a0.9583333333333334 0.9583333333333334 0 0 0 -0.9583333333333334 0.9583333333333334V13.416666666666668"
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="1"
+    ></path>
+    <path
+      d="m7.1875 3.354166666666667 1.9166666666666667 0"
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="1"
+    ></path>
+    <path
+      d="m9.104166666666668 6.229166666666667 6.708333333333334 0"
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="1"
+    ></path>
+    <path
+      d="m7.1875 9.104166666666668 8.625 0"
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="1"
+    ></path>
+    <path
+      d="m7.1875 11.979166666666668 8.625 0"
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="1"
+    ></path>
+  </svg>
+</template>
