454410d36ccf2ac65972cf891868b332a6cc9187
Margus Kevin Sünter <kevin@synter.ee>
2024-03-04 17:36:06 +0200

    Update router and add access check

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,53 @@
+import { useUserStore } from '@/stores/user'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/',
+      name: 'Index',
+      component: () => import('@/views/Index.vue'),
+    },
+    {
       path: '/login',
       name: 'Login',
       component: () => import('@/views/LoginView.vue'),
     },
     {
-      path: '/auth/callback',
-      name: 'AuthCallback',
-      component: () => import('@/views/AuthCallback.vue'),
+    {
+      path: "/:catchAll(.*)",
+      name: "NotFound",
+      component: () => import('@/views/404.vue'),
+      meta: {
+        public: true,
+      },
     },
   ],
+})
+
+router.beforeEach((to, from, next) => {
+  const user = useUserStore().user
+
+  // Check if logged in
+  if (!user) {
+    // If not logged in, put public route, then show
+    if (to.meta.public) {
+      return next()
+    }
+    // If route not public, redirect to login
+    return next('/login')
+  } else {
+
+    // If user is already logged in and trying to access public routes
+    // redirect to dashboard
+    if (to.meta.public) {
+      return next('/dashboard')
+    }
+
+    // Cases where user is logged in, always allow next
+    return next()
+  }
 })
 
 export default router
