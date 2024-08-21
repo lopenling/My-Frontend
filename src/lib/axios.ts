eb707e70e8ec939e19c740f6ac13b382f0147214
Margus Kevin Sünter <kevin@synter.ee>
2024-08-21 13:10:45 +0300

    Redirect user to login, when session has expired

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,14 @@
 import Axios from 'axios'
 
+Axios.interceptors.response.use(null, function (error) {
+  // If session has expired, redirect user to login page
+  if (error.response?.status === 401) {
+    window.location.href = '/login'
+  }
+
+  return Promise.reject(error)
+})
+
 export const axios = Axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
