c41feeffb7475b08646a063ad116879a81122f91
Margus Kevin Sünter <kevin@synter.ee>
2024-08-21 22:57:35 +0300

    Update CardHeaderButton to match design

--- src/components/CardHeader/CardHeaderButton.vue
+++ src/components/CardHeader/CardHeaderButton.vue
@@ -5,17 +5,23 @@
     class="flex items-center gap-2 rounded-md bg-primary-600 px-2.5 py-1.5 text-left text-sm font-semibold text-white shadow-sm transition hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
   >
     <component
-      :is="iconComponent"
-      class="h-3 w-3 flex-shrink-0 text-secondary-300/70"
-      data-stroke-width="2.5"
-      data-expand-by="1"
+      :is="props.iconComponent"
+      v-if="props.iconComponent"
+      class="flex-shrink-0 text-secondary-300/70"
+      :class="[props.iconSizeClass ? props.iconSizeClass : 'size-4', props.iconClass]"
+      :data-stroke-width="iconStrokeWidth || 2"
+      :data-expand-by="iconExpandBy"
     />
     <slot></slot>
   </button>
 </template>
 
 <script setup lang="ts">
-defineProps<{
-  iconComponent: object
-}>()
+const props = defineProps<{
+  iconComponent?: object;
+  iconSizeClass?: string;
+  iconClass?: string;
+  iconStrokeWidth?: number;
+  iconExpandBy?: number;
+}>();
 </script>
