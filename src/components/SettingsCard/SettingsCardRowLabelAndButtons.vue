479c76d675145459e09fdf89f7f96f4922a0d507
Margus Kevin Sünter <kevin@synter.ee>
2024-09-02 20:59:18 +0300

    Add SettingsCard button subcomponents

---
+++ src/components/SettingsCard/SettingsCardRowLabelAndButtons.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex items-center justify-between gap-2 px-4 text-sm text-stone-700">
+    <div class="py-4">
+      <slot name="default" />
+    </div>
+
+    <div class="flex gap-2">
+      <slot name="buttons" />
+    </div>
+  </div>
+</template>
---
+++ src/components/SettingsCard/SettingsCardSubtleButton.vue
@@ -0,0 +1,9 @@
+<template>
+  <button
+    v-bind="$attrs"
+    type="button"
+    class="rounded-md px-2.5 py-1.5 text-sm text-stone-400/80 transition hover:bg-stone-100 hover:text-stone-700 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary-600 focus-visible:ring-offset-0"
+  >
+    <slot></slot>
+  </button>
+</template>
