6e770fde0b983d146b975cbe434a28f4753cedc6
Margus Kevin Sünter <kevin@synter.ee>
2024-09-19 14:44:10 +0300

    Revert wrong commit of admin section int eams

--- src/components/Teams/TeamsAdministratorRole.vue
+++ src/components/Teams/TeamsAdministratorRole.vue
@@ -1,117 +1,40 @@
-import { defineStore } from 'pinia'
-import { axios } from '@/lib/axios'
+<template>
+  <CardHeader>
+    <CardHeaderHeading>Administrator role</CardHeaderHeading>
 
-export type Team = {
-  name: string
-  id: number
-  role: 'admin' | 'member'
+    <template #button>
+      <CardHeaderButton :icon-component="IconAdd" @click="newTeam"> New team </CardHeaderButton>
+    </template>
+  </CardHeader>
+  <SettingsCard class="divide-y">
+    <SettingsCardRowRoute
+      v-for="team in teamsStore.adminTeams"
+      :key="team.id"
+      :url="`/teams/${team.id}`"
+    >
+      {{ team.name }}
+    </SettingsCardRowRoute>
+    <SettingsCardRowMessage v-if="!teamsStore.hasAdminTeams">
+      No teams created yet
+    </SettingsCardRowMessage>
+  </SettingsCard>
+</template>
+
+<script setup lang="ts">
+import CardHeader from '@/components/CardHeader/CardHeader.vue'
+import CardHeaderHeading from '@/components/CardHeader/CardHeaderHeading.vue'
+import CardHeaderButton from '@/components/CardHeader/CardHeaderButton.vue'
+import IconAdd from '@/components/icons/streamline/regular/IconAdd.vue'
+import { useModalsStore } from '@/stores/modals'
+import SettingsCard from '@/components/SettingsCard/SettingsCard.vue'
+import SettingsCardRowRoute from '@/components/SettingsCard/SettingsCardRowRoute.vue'
+import { useTeamsStore } from '@/stores/teams'
+import SettingsCardRowMessage from '@/components/SettingsCard/SettingsCardRowMessage.vue'
+
+const modalStore = useModalsStore()
+const teamsStore = useTeamsStore()
+
+function newTeam() {
+  modalStore.newTeamModal = true
 }
-
-export type KeyedTeams = {
-  [key: number]: Team
-}
-
-export interface TeamStoreState {
-  teams: KeyedTeams
-}
-
-/**
- * Teams store
- */
-export const useTeamsStore = defineStore('teams', {
-  persist: true,
-
-  state: (): TeamStoreState => ({
-    teams: {},
-  }),
-
-  actions: {
-    /**
-     * Get all user teams
-     */
-    getTeams(): Promise<KeyedTeams> {
-      return axios.get('/v1/teams').then(({ data }) => {
-        // Map array of teams to object of teams
-        const keyedTeams: KeyedTeams = {}
-        data.forEach((team: Team) => {
-          keyedTeams[team.id] = team
-        })
-
-        this.teams = keyedTeams
-
-        return keyedTeams
-      })
-    },
-import { axios } from '@/lib/axios'
-
-export type Team = {
-  name: string
-  id: number
-  role: 'admin' | 'member'
-}
-
-export type KeyedTeams = {
-  [key: number]: Team
-}
-
-export interface TeamStoreState {
-  teams: KeyedTeams
-}
-
-/**
- * Teams store
- */
-export const useTeamsStore = defineStore('teams', {
-  persist: true,
-
-  state: (): TeamStoreState => ({
-    teams: {},
-  }),
-
-  actions: {
-    /**
-     * Get all user teams
-     */
-    getTeams(): Promise<KeyedTeams> {
-      return axios.get('/v1/teams').then(({ data }) => {
-        // Map array of teams to object of teams
-        const keyedTeams: KeyedTeams = {}
-        data.forEach((team: Team) => {
-          keyedTeams[team.id] = team
-        })
-
-        this.teams = keyedTeams
-
-        return keyedTeams
-      })
-    },
-
-    /**
-     * Create new team
-     *
-     * @param team
-     */
-    createTeam(team: Pick<Team, 'name'>): Promise<Team> {
-      return axios.post('/v1/teams', team).then(({ data }) => data)
-    },
-  },
-  getters: {
-    /**
-     * Get only teams, where user is admin
-     */
-    adminTeams(): KeyedTeams {
-      return Object.fromEntries(
-        Object.entries(this.teams).filter(([, value]) => value.role === 'admin'),
-      )
-    },
-
-    /**
-     * Get only teams, where user is member
-     */
-    memberTeams(): KeyedTeams {
-      return Object.fromEntries(
-        Object.entries(this.teams).filter(([, value]) => value.role === 'member'),
-      )
-    },
-  },
-})
+</script>
