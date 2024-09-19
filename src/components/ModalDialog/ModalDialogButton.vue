e71f01322e68fef2268b456167d5647bc7466d80
Margus Kevin Sünter <kevin@synter.ee>
2024-09-19 13:46:12 +0300

    Add modal to create teams

--- src/components/ModalDialog/ModalDialogButton.vue
+++ src/components/ModalDialog/ModalDialogButton.vue
@@ -3,11 +3,11 @@
     type="button"
     :class="[
       'inline-flex w-full justify-center rounded-md px-3.5 py-2 text-sm font-semibold shadow-sm transition focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 sm:w-auto',
-      props.appearance == 'primary' &&
+      appearance == 'primary' &&
         'bg-primary-600 text-white hover:bg-primary-500 focus-visible:outline-primary-600',
-      props.appearance == 'danger' &&
+      appearance == 'danger' &&
         'bg-red-600 text-white hover:bg-red-500 focus-visible:outline-red-600',
-      props.appearance == 'default' || !props.appearance
+      appearance == 'default' || !appearance
         ? 'bg-white text-stone-900 ring-1 ring-inset ring-stone-300 hover:bg-stone-100 focus-visible:outline-primary-600'
         : '',
       wideButtons && 'sm:shirnk sm:min-w-max sm:grow sm:basis-0',
---
+++ src/components/Teams/TeamsModalNewTeam.vue
@@ -0,0 +1,60 @@
+<template>
+  <ModalDialog
+    :show="!!modalsStore.newTeamModal"
+    name="newTeamModal"
+    max-width="md"
+    enable-initial-focus="smAndUp"
+  >
+    <form
+      id="newTeamForm"
+      class="mt-4 flex flex-col gap-x-4 gap-y-2.5 text-left"
+      @submit.prevent="submit"
+    >
+      <BaseInputText
+        id="teamName"
+        v-model="teamName"
+        appearance="white"
+        type="text"
+        name="teamName"
+        inputmode="text"
+        pattern="[a-zA-Z0-9_\-\(\)][a-zA-Z0-9_\-\(\) ]{1,49}"
+        autocomplete="off"
+        title="2-50 letters, no special characters except _ - ( )"
+        required
+      />
+    </form>
+
+    <template #title>Give the Team a Name</template>
+    <template #buttons>
+      <ModalDialogButton appearance="primary" :close-modal="false" type="submit" form="newTeamForm">
+        Create team
+      </ModalDialogButton>
+    </template>
+  </ModalDialog>
+</template>
+
+<script setup lang="ts">
+import { useModalsStore } from '@/stores/modals'
+import { useTeamsStore } from '@/stores/teams'
+import { ref } from 'vue'
+import BaseInputText from '../BaseInputText/BaseInputText.vue'
+import ModalDialog from '../ModalDialog/ModalDialog.vue'
+import ModalDialogButton from '../ModalDialog/ModalDialogButton.vue'
+
+const modalsStore = useModalsStore()
+const teamsStore = useTeamsStore()
+
+let teamName = ref('')
+
+function submit() {
+  teamsStore
+    .createTeam({
+      name: teamName.value,
+    })
+    .then(() => {
+      modalsStore.newTeamModal = false
+      teamName.value = ''
+      teamsStore.getTeams()
+    })
+}
+</script>
