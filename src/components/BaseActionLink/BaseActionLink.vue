a1567f821720399ae9ff4378e9a239a93b76bbb9
Margus Kevin Sünter <kevin@synter.ee>
2024-09-20 01:25:49 +0300

    Add base action link

---
+++ src/components/BaseActionLink/BaseActionLink.vue
@@ -0,0 +1,8 @@
+<template>
+  <button
+    type="button"
+    class="-mx-1 inline-flex rounded px-1 leading-normal text-primary-600 underline decoration-transparent decoration-1 underline-offset-[3px] transition hover:decoration-primary-400 focus-visible:decoration-transparent focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary-600 focus-visible:ring-offset-0"
+  >
+    <slot name="default" />
+  </button>
+</template>
