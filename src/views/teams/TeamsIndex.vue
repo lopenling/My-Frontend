a744bfb266d16b17cf83ba201fe4c88d65fc7b12
Margus Kevin Sünter <kevin@synter.ee>
2024-09-19 14:44:45 +0300

    Update teams index page

--- src/views/teams/TeamsIndex.vue
+++ src/views/teams/TeamsIndex.vue
@@ -3,16 +3,21 @@
     <TeamsHeader></TeamsHeader>
 
     <TeamsAdministratorRole></TeamsAdministratorRole>
-    <TeamsNewModal />
+    <TeamsMemberRole></TeamsMemberRole>
+
+    <TeamsModalNewTeam />
+    <TeamsModalLeaveTeam />
   </LayoutSettings>
 </template>
 
 <script setup lang="ts">
 import LayoutSettings from '@/layouts/LayoutSettings.vue'
-import TeamsAdministratorRole from '@/views/teams/TeamsAdministratorRole.vue'
-import TeamsHeader from '@/views/teams/TeamsHeader.vue'
+import TeamsAdministratorRole from '@/components/Teams/TeamsAdministratorRole.vue'
+import TeamsHeader from '@/components/Teams/TeamsHeader.vue'
 import { useTeamsStore } from '@/stores/teams'
-import TeamsNewModal from '@/views/teams/TeamsNewModal.vue'
+import TeamsModalNewTeam from '@/components/Teams/TeamsModalNewTeam.vue'
+import TeamsMemberRole from '@/components/Teams/TeamsMemberRole.vue'
+import TeamsModalLeaveTeam from '@/components/Teams/TeamsModalLeaveTeam.vue'
 
 const teamsStore = useTeamsStore()
 teamsStore.getTeams()
