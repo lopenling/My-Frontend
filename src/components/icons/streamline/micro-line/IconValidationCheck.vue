46a0e9a66124cfa951c3f11380a7997aca6df171
Margus Kevin Sünter <kevin@synter.ee>
2024-09-20 14:35:20 +0300

    Add two icons: ValidationCheck and WarningCircle

---
+++ src/components/icons/streamline/micro-line/IconValidationCheck.vue
@@ -0,0 +1,34 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" :viewBox="viewBoxValue">
+    <desc>Validation Check Streamline Icon: https://streamlinehq.com</desc>
+    <path
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      d="M1.125 14.0625 5.0512500000000005 18.900000000000002a2.1847499999999997 2.1847499999999997 0 0 0 3.375 -0.020249999999999997L21.375 2.8125"
+      :stroke-width="strokeWidth"
+    ></path>
+  </svg>
+</template>
+
+<script setup lang="ts">
+import { useAttrs } from 'vue'
+
+const attrs = useAttrs()
+let viewBoxValue = '0 0 24 24'
+let strokeWidth = '1.5'
+
+if (attrs['data-expand-by']) {
+  viewBoxValue = viewBoxValue
+    .split(' ')
+    .map((value, index) => {
+      if (index < 2) {
+        return parseInt(value) - Number(attrs['data-expand-by'])
+      } else {
+        return parseInt(value) + Number(attrs['data-expand-by']) * 2
+      }
+    })
+    .join(' ')
+}
+if (attrs['data-stroke-width']) strokeWidth = attrs['data-stroke-width'].toString()
+</script>
---
+++ src/components/icons/streamline/micro-line/IconWarningCircle.vue
@@ -0,0 +1,55 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" :viewBox="viewBoxValue">
+    <desc>Warning Circle Streamline Icon: https://streamlinehq.com</desc>
+    <path
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      d="M11.25 21.375a10.125 10.125 0 1 0 0 -20.25 10.125 10.125 0 0 0 0 20.25Z"
+      :stroke-width="strokeWidth"
+    ></path>
+    <path
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      d="M11.25 6.1875v4.5"
+      :stroke-width="strokeWidth"
+    ></path>
+    <path
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      d="M11.25 16.3035a0.5625 0.5625 0 0 1 0 -1.125"
+      :stroke-width="strokeWidth"
+    ></path>
+    <path
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      d="M11.25 16.3035a0.5625 0.5625 0 0 0 0 -1.125"
+      :stroke-width="strokeWidth"
+    ></path>
+  </svg>
+</template>
+
+<script setup lang="ts">
+import { useAttrs } from 'vue'
+
+const attrs = useAttrs()
+let viewBoxValue = '-0.75 -0.75 24 24'
+let strokeWidth = '1.5'
+
+if (attrs['data-expand-by']) {
+  viewBoxValue = viewBoxValue
+    .split(' ')
+    .map((value, index) => {
+      if (index < 2) {
+        return parseInt(value) - Number(attrs['data-expand-by'])
+      } else {
+        return parseInt(value) + Number(attrs['data-expand-by']) * 2
+      }
+    })
+    .join(' ')
+}
+if (attrs['data-stroke-width']) strokeWidth = attrs['data-stroke-width'].toString()
+</script>
